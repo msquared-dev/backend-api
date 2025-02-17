8ae528ce1037a39f25ca4e25ef7d9586e57b1976
EugeneShalli <shaluginjeka@gmail.com>
2025-02-17 11:05:57 +0100

    added nest_asyncio

--- services/bots_orchestrator.py
+++ services/bots_orchestrator.py
@@ -191,7 +191,8 @@ class BotsManager:
 
                     import json
                     # asyncio.new_event_loop().run_until_complete(run_full_report(client))
-                    full_report = client.full_report()
+                    full_report = asyncio.new_event_loop().run_until_complete(self.run_full_report(client))
+                    # full_report = client.full_report()
 
                     full_report = json.loads(full_report.report)
 
